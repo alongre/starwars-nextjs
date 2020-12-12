7bf3297df83297bf2e2fd7162cb07dbbe9119918
alongre <alongre@gmail.com>
2020-12-12 16:31:46 +0200

    removed deleted lines

--- components/DarkSwitch.tsx
+++ components/DarkSwitch.tsx
@@ -5,12 +5,6 @@ import { Theme, THEME_KEY, ThemeContext } from '../contexts/ThemeContext'
 
 const DarkSwitch = () => {
 	const { theme, setTheme } = useContext(ThemeContext)
-	// const [darkMode, setDarkMode] = useState<Theme>(() => theme)
-
-	// useEffect(() => {
-	// 	setTheme(darkMode)
-	// }, [darkMode])
-
 	return (
 		<button className='w-4 h-4' onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
 			{theme === 'dark' ? <FaMoon /> : <FaSun />}
